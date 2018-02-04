7738453ba09a6afee9e2e1b5614b64766c9a6850
Chet Simpson <3150309+ChetSimpson@users.noreply.github.com>
2018-02-04 08:09:24 -0800

    Fixed builder for contact offline status so it provides the correct keypairs allowing the status in the buddy list to be updated.

--- Protocols/YMsg/include/Protocols/YMsg/Server/Builders/ContactOffline.h
+++ Protocols/YMsg/include/Protocols/YMsg/Server/Builders/ContactOffline.h
@@ -19,7 +19,9 @@ namespace Giblet { namespace Protocols { namespace YMsg { namespace Server { nam
 		virtual void Build(session_type& session, string_view_type contactId)
 		{
 			Initialize(session, ServiceId, AttributeId);
-			Append(Keys::Unknown8, "0");
+			Append(Keys::Unknown10, "0");
+			Append(Keys::Unknown13, "0");
+			Append(Keys::Unknown47, "2");
 			Append(Keys::ContactId, contactId);
 		}
 
@@ -29,7 +31,9 @@ namespace Giblet { namespace Protocols { namespace YMsg { namespace Server { nam
 		struct Keys
 		{
 			static const key_type ContactId = 7;
-			static const key_type Unknown8 = 8;	//	Must be "0"?
+			static const key_type Unknown10 = 10;	//	Must be "0"?
+			static const key_type Unknown13 = 13;	//	Must be "0"?
+			static const key_type Unknown47 = 47;	//	Must be "2"?
 		};
 
 		static const serviceid_type ServiceId = 2;
